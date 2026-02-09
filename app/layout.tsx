d149c03ef5353bbc5c518c37868e7950778b0059
pattana supachaimongkon <pattanasupachaimongkon@pattanas-MacBook-Pro.local>
2026-02-09 13:46:14 +0700

    add menifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Playpen_Sans_Thai } from "next/font/google";
 import "./globals.css";
 
@@ -11,6 +11,20 @@ const playpen = Playpen_Sans_Thai({
 export const metadata: Metadata = {
   title: "Happy Valentine's day",
   description: "Happy Valentine's day 2026 For Zumoo",
+  manifest: "/manifest.json", // 2. เชื่อมไฟล์ manifest
+  appleWebApp: { // 3. ตั้งค่าสำหรับ iPhone (iOS)
+    capable: true,
+    statusBarStyle: "black-translucent",
+    title: "Happy Valentine's day",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#f43f5e",
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false, // ห้ามซูม (เพื่อให้เหมือนแอป)
 };
 
 export default function RootLayout({
